20138ee83e012ec429d9fb9e96a9595ca5f05e69
Joseph Delong <joseph@delong.me>
2020-01-29 12:32:40 -0600

    updates to simulator

--- script.py
+++ script.py
@@ -1,15 +1,22 @@
 from enum import Enum
 
+# configuration
+SHARD_COUNT = 64;
+
+# data types
 class TransactionFragmentType(Enum):
-	ACCT_LOOKUP = 1
-	PAYMENT_FROM_ACCT = 2
-	CONTRACT_CALL = 3
+	PAYMENT_TO_ACCOUNT = 1
+	PAYMENT_TO_SHARD = 2
+	CONTRACT_WRITE = 3
+	CONTRACT_READ = 4
 
-class TransactionFragment:
-	isForeignShard = type(False)
-	type = TransactionFragmentType(1)
-
-txnFragment = TransactionFragment()
+class TransactionFragment:	
+	def __init__(self, is_foreign_shard, type):
+		self.is_foreign_shard = is_foreign_shard
+		self.type = type
+		
+# application
+txnFragment = TransactionFragment(type(False), TransactionFragmentType.PAYMENT_TO_ACCOUNT)
 
 print(txnFragment.type)
 	
