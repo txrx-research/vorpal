6309e810a94670a782b46bfa1b948a0cbd8af3c7
Joseph Delong <joseph@delong.me>
2020-01-30 15:31:16 -0600

    README.md revisions

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ In this simulation we will model the execution of cross shard transactions the E
 	- Transaction Throughput
 	- Transaction Size
 	- Wait time for finality  
+
+### Simulation Notes
+A problem in simulating cross-shard transactions is the cross contract calls. In Eth1 cross contract calls are synchronous, when a method is called on a deployed contract that references an external contract the transaction is processed synchronously without discontinuity. Continuity within the execution is guaranteed by the block producer having absolute control of the state. Within a sharded system the shard block producer only has control of the state within their shard. If a contract method call makes a cross contract call to mutate state on a foreign shard execution becomes discontiuous. The following structure of a traditional Eth1 transaction is unsuitable for the purposes of simulating a cross-shard transaction:
+
+```
+{
+	from: 20 byte address,
+	to: 20 byte address,
+	value: wei,
+	input: bytecode,
+	gas:
+
+}
+```
+
+The discontinuity of cross-shard transactions requires a new format to allow the simulation to orchestrate a pause and resume within a single transaction. The following structure will allow such an interaction:
+
+```
+{
+
+}
+```
--- script.py
+++ script.py
@@ -1,22 +1,68 @@
 from enum import Enum
+import random
 
 # configuration
 SHARD_COUNT = 64;
 
 # data types
 class TransactionFragmentType(Enum):
-	PAYMENT_TO_ACCOUNT = 1
-	PAYMENT_TO_SHARD = 2
-	CONTRACT_WRITE = 3
-	CONTRACT_READ = 4
+	TO_EOA = 1
+	TO_CONTRACT = 2
+	TO_SHARD = 3
+	CONTRACT_DEPLOY = 4
+	CONTRACT_WRITE = 5
+	CONTRACT_TO_CONTRACT = 6
 
 class TransactionFragment:	
 	def __init__(self, is_foreign_shard, type):
 		self.is_foreign_shard = is_foreign_shard
 		self.type = type
-		
-# application
-txnFragment = TransactionFragment(type(False), TransactionFragmentType.PAYMENT_TO_ACCOUNT)
 
-print(txnFragment.type)
+class ReportFragment:
+	gas_cost = None
+	#milliseconds
+	time = None
+
+def generateRandomTransaction(size):
+	transaction = list()
+	for i in range(size):
+		isForeign = random.choice([True, False])
+		txnFragmentType =  random.choice([1, 2, 3, 4])
+		txnFragment = TransactionFragment(isForeign, TransactionFragmentType(txnFragmentType))
+		transaction.append(txnFragment)
+	return transaction
+
+def simulation(transaction, strategy):
+	report = list();
+	# mempool time
+	for txnFragment in transaction:
+		orchReportFragment = orchcestrationTiming(txnFragment, ReportFragment());
+		reportFragment = strategy(txnFragment, ReportFragment())
+		report.append(reportFragment)
+	return report
+
+def receipt(transactionFragment, reportFragment):
+	# orchestration timing
+	if(transactionFragment.is_foreign_shard){
+
+	}
+	# execution timing
+	if(txnFragment.type == TransactionFragment.PAYMENT_TO_ACCOUNT):
+
+	else if(txnFragment.type == TransactionFragment.PAYMENT_TO_SHARD):
+
+	else if(txnFragment.type == TransactionFragment.PAYMENT_TO_SHARD):
+
+	else if(txnFragment.type == TransactionFragment.PAYMENT_TO_SHARD):
+
+	return reportFragment;
+# application
+
+# txnFragment = TransactionFragment(type(False), TransactionFragmentType.PAYMENT_TO_ACCOUNT)
+
+# print(txnFragment.type)
+
+transaction = generateRandomTransaction(5);
+print(transaction[2].type)
+
 	
